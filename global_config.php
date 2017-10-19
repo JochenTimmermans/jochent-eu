974b4bf8b2d4a98262507a3bde3c4350140c8300
jfreeman82 <jfreeman@skedaddling.com>
2017-10-19 16:44:06 +0200

    local config file for easy server migration

--- global_config.php
+++ global_config.php
@@ -4,8 +4,12 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 
-define('BASE_URL', 'https://btc.islandshore.net/');
 define('ROOT_URL', $_SERVER['DOCUMENT_ROOT'].'/../');
+
+if (!file_exists(ROOT_URL.'config.local.php')) {
+    die(ROOT_URL."config.local not found.");
+}
+require ROOT_URL.'config.local.php';
 define('WWW', BASE_URL);
 
 define("MYSQL_HOST","localhost");
