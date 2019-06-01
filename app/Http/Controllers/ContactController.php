56655f36f65cfa620f4b7f2f16e50ebd6dda4ef0
jfreeman <jfreeman@skedaddling.com>
2019-06-01 14:39:09 +0000

    Fresh Laravel install
    Laravel was installed afresh. The old site is hiding in the old/ folder,
    feeling depressed.

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Message;
 use Illuminate\Http\Request;
+use Mail;
 
 class ContactController extends Controller
 {
     public function index()
     {
         return view('contact');
+    }
+
+    public function post(Request $request)
+    {
+        $validatedData = $request->validate([
+            'user_name' => 'required|max:100',
+            'user_email' => 'required|email|max:100',
+            'subject' => 'required|max:100',
+            'message' => 'required'
+        ]);
+
+//        $message = Message::create($validatedData);
+        $message = new \App\Message;
+        $message->user_name = $request->input('user_name');
+        $message->user_email = $request->input('user_email');
+        $message->subject = $request->input('subject');
+        $message->message = $request->input('message');
+        $message->save();
+
+        // Send Email
+        $data = array('newmsg' => $message);
+
+        Mail::send('emails.notify', $data, function($email) {
+            $email->to(env('EMAIL_ADMIN'), 'Jochen T.')
+                    ->from('jochen@jochent.eu','MailerDaemon')
+                    ->subject('JochenT.eu: New Message');
+        });
+
+        return view('success');
     }
 }
---
+++ resources/views/emails/notify.blade.php
@@ -0,0 +1,6 @@
+<h3>A New Message Was Submitted</h3>
+
+<div>By: <b>{{ $newmsg->user_name }}</b><small>({{ $newmsg->user_email }})</small></div>
+<div id="subject">{{ $newmsg->subject }}</div>
+<div id="message">{{ $newmsg->message }}</div>
+
