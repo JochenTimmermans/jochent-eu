cb45bcff2c3b1ee3b79258da15a78d09898eb982
jfreeman <jfreeman@skedaddling.com>
2019-06-01 16:48:53 +0000

    Resolve "Bio in JSON style"

--- resources/views/partials/aboutme.blade.php
+++ resources/views/partials/aboutme.blade.php
@@ -1,15 +1,51 @@
 <div id="aboutme">
     <div class="bio">
         <h2 class="title">Bio</h2>
+        <span class="brackets brackets-open">{</span>
         <table>
-            <tr><td>Name</td><td>Jochen Timmermans</td></tr>
-            <tr><td>Location</td><td>Brussels, Belgium</td></tr>
-            <tr><td>Born</td><td>{{ $admin['date_of_birth'] }} ({{ $admin['age']  }} years old)</td></tr>
-            <tr><td>Current Employment</td><td>PHP Developer @ <a href="https://striktlysoftware.com/">Striktly Business Software</a> since June 27, 2018</td></tr>
-            <tr><td>Languages</td><td>PHP7, Python, Javascript/JQuery, NodeJS, SASS, HTML, CSS, SQL</td></tr>
-            <tr><td>Frameworks</td><td>Laravel, CodeIgniter</td></tr>
-            <tr><td>Databases</td><td>MySQL/mariadb, sqlite3, MongoDB</td></tr>
-            <tr><td>Servers</td><td>Linux (debian), Apache, docker, Nginx, virtualbox</td></tr></tr>
+            <tr>
+                <td class="key">Name</td>
+                <td class="colon">:</td>
+                <td class="value">Jochen Timmermans</td>
+            </tr>
+            <tr>
+                <td class="key">Location</td>
+                <td class="colon">:</td>
+                <td class="value">Brussels, Belgium</td>
+            </tr>
+            <tr>
+                <td class="key">Born</td>
+                <td class="colon">:</td>
+                <td class="value">{{ $admin['date_of_birth'] }} ({{ $admin['age']  }} years old)</td>
+            </tr>
+            <tr>
+                <td class="key">Current Employment</td>
+                <td class="colon">:</td>
+                <td class="value">PHP Developer @ <a href="https://striktlysoftware.com/">Striktly Business Software</a> since June
+                    27, 2018
+                </td>
+            </tr>
+            <tr>
+                <td class="key">Languages</td>
+                <td class="colon">:</td>
+                <td class="value">PHP7, Python, Javascript/JQuery, NodeJS, SASS, HTML, CSS, SQL</td>
+            </tr>
+            <tr>
+                <td class="key">Frameworks</td>
+                <td class="colon">:</td>
+                <td class="value">Laravel, CodeIgniter</td>
+            </tr>
+            <tr>
+                <td class="key">Databases</td>
+                <td class="colon">:</td>
+                <td class="value">MySQL/mariadb, sqlite3, MongoDB</td>
+            </tr>
+            <tr>
+                <td class="key">Servers</td>
+                <td class="colon">:</td>
+                <td class="value">Linux (debian), Apache, docker, Nginx, virtualbox</td>
+            </tr>
         </table>
+        <span class="brackets brackets-close">}</span>
     </div>
 </div>
